81028f69832abd45dddddeba03f871ce2978c9cb
Maik Messerschmidt <maik.messerschmidt@gmx.net>
2023-12-16 19:26:07 +0100

    Solved puzzle 4 part 2.

--- src/Aoc2023/Puzzle4.hs
+++ src/Aoc2023/Puzzle4.hs
@@ -1,12 +1,18 @@
 module Aoc2023.Puzzle4 where
 
 import Control.Applicative ((<|>))
+import Control.Monad.State.Strict (State, get, put, runState, execState)
+import Control.Monad
 import Data.Char (isDigit)
 import Data.Functor ((<&>))
 import Data.List (foldl')
-import Text.ParserCombinators.ReadP
+import Text.ParserCombinators.ReadP hiding (get)
 
-import Data.Set (Set, intersection, size, fromList)
+import Data.Map.Strict (Map, (!))
+import Data.Set (Set)
+import qualified Data.Map.Strict as Map
+import qualified Data.Set as Set
+
 
 data Card = Card Int (Set Int) (Set Int) deriving (Read, Show)
 
@@ -46,13 +52,36 @@ card = do
   _ <- string "|"
   skipSpaces
   drawn <- natural `sepBy` many1 (char ' ')
-  return $ Card cardId (fromList winning) (fromList drawn)
+  return $ Card cardId (Set.fromList winning) (Set.fromList drawn)
+
+matching :: Card -> Int
+matching (Card _ winning drawn) = Set.size $ winning `Set.intersection` drawn
 
 score :: Card -> Int
-score (Card _ winning drawn)
-  = case size $ winning `intersection` drawn of
-    0 -> 0
-    n -> 2 ^ (n - 1)
+score c
+    | n == 0    = 0
+    | otherwise = 2 ^ (n - 1)
+  where
+    n = matching c
+
+toMap :: [Card] -> Map Int (Card, Int)
+toMap cards = Map.fromList [(cardId, (c, 1)) | c@(Card cardId _ _) <- cards]
+
+grow :: State (Map Int (Card, Int)) ()
+grow = do
+  ks <- Map.keys <$> get
+
+  forM_ ks $ \k -> do
+    m <- get
+    let x :: Map Int (Card, Int)
+        x = m
+    let (card, count) = (m ! k)
+        n = matching card
+        ids = [k+1..k+n]
+
+    forM_ ids $ \k' -> do
+      m' <- get
+      put $ Map.adjust (\(card', count') -> (card', count' + count)) k' m'
 
 -- part1 :: String -> Integer
 part1 content = sum $ score <$> cards
@@ -60,5 +89,8 @@ part1 content = sum $ score <$> cards
     parser = (card `sepBy` newline <* optional newline <* eof)
     cards = fst $ head $ readP_to_S parser content
 
-part2 :: String -> Integer
-part2 content = undefined
+part2 :: String -> Int
+part2 content = sum [n | (Card _ _ _, n) <- Map.elems $ execState grow $ toMap cards ]
+    where
+      parser = (card `sepBy` newline <* optional newline <* eof)
+      cards = fst $ head $ readP_to_S parser content
